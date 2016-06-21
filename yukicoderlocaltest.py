2e87e8865abf5d3b0389b5962b79d988aa7910be
ebicochineal <yz94588@gmail.com>
2016-06-22 07:00:39 +0900

    fix

--- yukicoderlocaltest.py
+++ yukicoderlocaltest.py
@@ -10,14 +10,14 @@ from datetime import datetime, timedelta
 from zipfile import ZipFile, ZIP_STORED
 from subprocess import Popen, PIPE
 
-g_crdir = os.path.dirname(__file__) + "/"
+g_crdir = os.path.dirname(__file__).replace("\\", "/") + "/"
 g_testdir = "testcase/"
 g_sampledir = "samplecase/"
 g_builddir = "build/"
 g_in = "test_in/"
 g_out = "test_out/"
 # g_compilerpath = "C:/MinGW/bin" + ";" + "C:/Program Files (x86)/MSBuild/14.0/bin"
-g_timeout = 5
+g_timeout = 2
 g_cmdc = {}
 g_cmdi = {}
 g_cls = ""
@@ -42,13 +42,13 @@ def setenv():
         g_cls = "clear"
 
 class Test():
-    def __init__(self, num, ext, progpath):
+    def __init__(self, num, ext, prog, case):
         cmd = g_cmdi[ext] if ext in g_cmdi else ["[i]"]
-        self.cmd = raplace_cmd(cmd, progpath)
+        self.cmd = cmdio(cmd, prog)
         self.data = {}
-        self.zippath = g_crdir + g_testdir + zipname(num)
+        self.case = case
         self.filelist = []
-        with ZipFile(self.zippath, 'r') as z:
+        with ZipFile(self.case, 'r') as z:
             self.filelist = sorted([x.split(g_in)[1] for x in z.namelist() if g_in in x])
     def tests(self):
         for i in self.filelist:
@@ -80,7 +80,7 @@ class Test():
             result = [yellow("TLE"), "-----", din2k, dout2k, ""]
         return result
     def read(self, path):
-        with ZipFile(self.zippath, 'r') as z:
+        with ZipFile(self.case, 'r') as z:
             return z.read(path).decode("utf-8")
 
 def jadge(v1, v2):
@@ -110,7 +110,76 @@ def lenfixed(s, n):
         s = s[:n-3] + "..."
     return ("{: <" + str(n) + "}").format(s)
 
-def tolist(s, w, h):
+def zipname(num):
+    return "No{:0>4}.zip".format(num)
+
+def trimsample(s):
+    if len(s) > 0:
+        if s[-1] != "\n":
+            s += "\n"
+        if s[0] == "\n":
+            s = s[1:]
+        return s
+    else:
+        return s
+
+def cmdio(cmd, prog):
+    r = []
+    for i in cmd:
+        i = i.replace("[i]", prog)
+        i = i.replace("[o]", g_crdir + g_builddir + "test.exe")
+        r += [i]
+    return r
+
+def path_to_nep(s):
+    s = s.replace('"', '')
+    basename = os.path.basename(s)
+    cwd = os.getcwd() if os.path.dirname(s) == "" else ""
+    prog = (cwd + s).replace("\\", "/")
+    ext = to_ext(basename)
+    num = to_num(basename)
+    return (num, ext, prog)
+
+def view_ior(test):
+    n = 0
+    while 1:
+        i = test.filelist[n]
+        tw, th = os.get_terminal_size()
+        w = tw // 3 - 1
+        h = th - 6
+        lin = to_list(test.data[i][2], w, h)
+        lout = to_list(test.data[i][3], w, h)
+        lprg = to_list(test.data[i][4], w, h)
+        os.system(g_cls)
+        print(test.data[i][0], test.data[i][1])
+        print(" ".join([lenfixed("data_in", w), lenfixed("data_out", w), lenfixed("program_output", w)]))
+        for y in range(h):
+            print("|".join([lin[y], lout[y], lprg[y]]))
+        print("-"*(tw-1))
+        print(lenfixed("Next : [ENTER]     Quit : [Q]", w*2+2) + lenfixed(i, w))
+        s = input()
+        if s == "":
+            n = (n+1) % len(test.filelist)
+        if s == "q":
+            break
+
+def to_ext(s):
+    sp = s.split(".")
+    return sp[-1] if len(sp) > 1 else None
+
+def to_num(s):
+    num = ""
+    for i in s:
+        if i in "0123456789":
+            num += i
+        elif len(num) > 0:
+            if i == "." : break
+            num = ""
+    if len(num) > 4:
+        num = num[-4:]
+    return num if len(num) > 0 else None
+
+def to_list(s, w, h):
     r = []
     for i in s.split("\n"):
         if len(i) > w:
@@ -126,32 +195,20 @@ def tolist(s, w, h):
         r[i] = r[i] + " " * (w-len(r[i]))
     return r
 
-def zipname(num):
-    return "No{:0>4}.zip".format(num)
-    
-def problemnumber(filename):
-    num = ""
-    for i in filename:
-        if i in "0123456789":
-            num += i
-        elif len(num) > 0:
-            if i == "." : break
-            num = ""
-    if len(num) > 4:
-        num = num[-4:]
-    return num if len(num) > 0 else None
+def try_build(ext, prog):
+    try:
+        os.system(" ".join(cmdio(g_cmdc[ext], prog)))
+        return True
+    except:
+        return False
 
-def raplace_cmd(cmd, progpath):
-    r = []
-    for i in cmd:
-        i = i.replace("[i]", progpath)
-        i = i.replace("[o]", g_crdir + g_builddir + "test.exe")
-        r += [i]
-    return r
+def try_mkdir(dir):
+    try:
+        if not os.path.exists(dir) : os.mkdir(dir)
+    except:
+        pass
 
-def testcase_download(num):
-    if os.path.exists(g_crdir + g_testdir + zipname(num)) : return
-    print("TestCase Download...")
+def try_testcase_download(num):
     try:
         cj = http.cookiejar.LWPCookieJar()
         cj.load(g_crdir + "cookie.txt")
@@ -160,24 +217,21 @@ def testcase_download(num):
         with opener.open(url) as r:
             with open(g_crdir + g_testdir + zipname(num), "wb") as f:
                 f.write(r.read())
-        print("TestCase Download Completed")
+        return True
     except:
-        print("TestCase Download Failed")
+        return False
 
-def samplecase_download(num):
-    if os.path.exists(g_crdir + g_sampledir + zipname(num)) : return
-    print("SampleCase Download...")
+def try_samplecase_download(num):
     try:
         data_in_list = []
         data_out_list = []
         ht = urllib.request.urlopen('http://yukicoder.me/problems/no/' + num).read().decode('utf-8')
         for i in ht.split("<h6>入力</h6>")[1:]:
             s = i.split("<pre>")[1].split("</pre>")[0]
-            data_in_list += [s + "\n"]
+            data_in_list += [trimsample(s)]
         for i in ht.split("<h6>出力</h6>")[1:]:
             s = i.split("<pre>")[1].split("</pre>")[0]
-            data_out_list += [s + "\n"]
-        print("SampleCase Download Completed")
+            data_out_list += [trimsample(s)]
         sampledir = g_crdir + g_sampledir
         provisionaldir = sampledir + zipname(num).split(".")[0] + "/"
         try_mkdir(provisionaldir)
@@ -193,104 +247,96 @@ def samplecase_download(num):
                     f.write(data_out_list[i].encode('utf-8'))
                 z.write(provisionaldir + g_out + filename, g_out + filename)
         shutil.rmtree(provisionaldir)
+        return True
     except:
-        print("SampleCase Download Failed")
+        return False
 
-def testcase_test(test):
-    print("Run >>>", " ".join(test.cmd))
-    for i, j, k in test.tests():
-        print(i, j, k)
-    rm = g_crdir + g_builddir + "test.exe"
-    if os.path.exists(rm):
-        print("Remove >>>", g_builddir + "test.exe")
-        os.remove(rm)
-
-def testcase_view(test):
-    n = 0
-    while 1:
-        i = test.filelist[n]
-        tw, th = os.get_terminal_size()
-        w = tw // 3 - 1
-        h = th - 6
-        lin = tolist(test.data[i][2], w, h)
-        lout = tolist(test.data[i][3], w, h)
-        lprg = tolist(test.data[i][4], w, h)
-        os.system(g_cls)
-        print(test.data[i][0], test.data[i][1])
-        print(" ".join([lenfixed("data_in", w), lenfixed("data_out", w), lenfixed("program_output", w)]))
-        for y in range(h):
-            print("|".join([lin[y], lout[y], lprg[y]]))
-        print("-"*(tw-1))
-        print(lenfixed("Next : [ENTER]     Quit : [Q]", w*2+2) + lenfixed(i, w))
-        s = input()
-        if s == "":
-            n = (n+1) % len(test.filelist)
-        if s == "q":
-            break
-
-def program_select():
-    tpp = input("TestProgram Path = ").replace('"', '')
-    opd = os.path.dirname(tpp)
-    testprog = (os.getcwd() + tpp if opd == "" else tpp).replace("\\", "/")
-    opbsp = os.path.basename(tpp).split(".")
-    ext = opbsp[1] if len(opbsp) > 1 else None
-    num = problemnumber(testprog)
-    if num == None :
-        num = problemnumber(input("Problem Number = "))
-    if ext in g_cmdc:
-        g_cmdc[ext] = raplace_cmd(g_cmdc[ext], testprog)
-        print("Build >>>", " ".join(g_cmdc[ext]))
-        os.system(" ".join(g_cmdc[ext]))
-        ext = "exe"
-        testprog = g_crdir + g_builddir + "test.exe"
-    return (num, ext, testprog)
-
-def try_mkdir(dir):
-    try:
-        if not os.path.exists(dir) : os.mkdir(dir)
-    except:
-        pass
-
-def make_cookie():
-    if os.path.exists(g_crdir + "cookie.txt") : return
-    session = input("REVEL_SESSION = ")
+def try_makecookie(session):
     try:
         expires = (datetime.today() + timedelta(days=30)).strftime("%Y-%m-%d %H:%M:%S")
         with open(g_crdir + "cookie.txt", "w") as f:
             w = '#LWP-Cookies-2.0\nSet-Cookie3: REVEL_SESSION="%s"; path="/"; domain="yukicoder.me"; path_spec; expires="%s"; version=0'%(session, expires)
             f.write(w)
+        return True
     except:
-        pass
+        return False
 
-def view_quit_loop(test):
+def y_download(num):
+    if num == None : return
+    if not os.path.exists(g_crdir + g_testdir + zipname(num)) and os.path.exists(g_crdir + "cookie.txt"):
+        print("TestCase Download...")
+        if try_testcase_download(num):
+            print("TestCase Download Completed")
+        else:
+            print("TestCase Download Failed")
+    if not os.path.exists(g_crdir + g_sampledir + zipname(num)):
+        print("SampleCase Download...")
+        if try_samplecase_download(num):
+            print("SampleCase Download Completed")
+        else:
+            print("SampleCase Download Failed")
+
+def y_test(num, ext, prog, case):
+    t = Test(num, ext, prog, case)
+    print("Run >>>", " ".join(t.cmd))
+    for i, j, k in t.tests():
+        print(i, j, k)
     print("TestCase View : [ENTER]     Quit : [Q]")
     while 1:
-        s = input()
-        if s == "":
-            testcase_view(test)
+        c = input()
+        if c == "":
+            view_ior(t)
             break
-        if s == "q":
+        if c == "q":
             break
+
+def y_cookie():
+    if os.path.exists(g_crdir + "cookie.txt") : return
+    session = input("REVEL_SESSION = ")
+    if session != "":
+        if try_makecookie(session):
+            print("Make cookie.txt")
+        else:
+            print("Failed")
+    else:
+        print("Skip REVEL_SESSION")
+        print("SampleCase Only")
+
+def y_exeremove():
+    rm = g_crdir + g_builddir + "test.exe"
+    if os.path.exists(rm):
+        print("Remove >>>", g_builddir + "test.exe")
+        os.remove(rm)
 
 def main():
     os.system(g_cls)
-    try_mkdir(g_crdir + g_testdir)
-    try_mkdir(g_crdir + g_sampledir)
-    try_mkdir(g_crdir + g_builddir)
-    make_cookie()
-    if os.path.exists(g_crdir + "cookie.txt"):
-        num, ext, testprog = program_select()
-        if num != None:
-            testcase_download(num)
-            samplecase_download(num)
-    if num == None or ext == None or testprog == None:
-        return
-    if os.path.exists(g_crdir + g_testdir + zipname(num)) and os.path.exists(testprog):
-        t = Test(num, ext, testprog)
-        testcase_test(t)
-        view_quit_loop(t)
-    else:
-        pass
+    testdir = g_crdir + g_testdir
+    sampledir = g_crdir + g_sampledir
+    builddir = g_crdir + g_builddir
+    try_mkdir(testdir)
+    try_mkdir(sampledir)
+    try_mkdir(builddir)
+    y_cookie()
+    num, ext, prog = path_to_nep(input("TestProgram Path = "))
+    if ext in g_cmdc:
+        print("Build >>>", " ".join(cmdio(g_cmdc[ext], prog)))
+        if try_build(ext, prog):
+            ext = "exe"
+            prog = builddir + "test.exe"
+    if num == None:
+        num = to_num(input("Problem Number = "))
+    y_download(num)
+    if all([num, ext, prog]):
+        samplecase = sampledir + zipname(num)
+        testcase = testdir + zipname(num)
+        cs = os.path.exists(samplecase)
+        ct = os.path.exists(testcase)
+        cp = os.path.exists(prog)
+        if all([ct, cp]):
+            y_test(num, ext, prog, testcase)
+        if all([cs, not ct, cp]):
+            y_test(num, ext, prog, samplecase)
+    y_exeremove()
 
 if __name__ == '__main__':
     setenv()
